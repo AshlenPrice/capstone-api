7b973e6b49857e16e35a78bece7e056cc5f01d2c
ashlenprice <ashlen.price@gmail.com>
2017-03-27 15:00:41 -0400

    Scaffold Profile resource
    - Edited model and added activeRecord enum and a hash of the attributes
    for role
    - Edit actions in controller
    - Validated model
    - Validated constraints
    Next will be creating test scripts  and making sure it all works!

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+class ProfilesController < ApplicationController
+  before_action :set_profile, only: [:update]
+
+  # GET /profiles
+  def index
+    @profiles = Profile.all
+
+    render json: @profiles
+  end
+
+  # GET /profiles/1
+  def show
+    render json: Profile.find(params[:id])
+  end
+
+  # POST /profiles
+  def create
+    @profile = current_user.build_profile(profile_params)
+
+    if @profile.save
+      render json: @profile, status: :created
+    else
+      render json: @profile.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /profiles/1
+  def update
+    if @profile.update(profile_params)
+      head :no_content
+    else
+      render json: @profile.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /profiles/1
+  def destroy
+    @profile.destroy
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_profile
+    @profile = Profile.find_by(id: params[:id], user: current_user)
+  end
+  private :set_profile
+
+  # Only allow a trusted parameter "white list" through.
+  def profile_params
+    params.require(:profile).permit(:given_name, :family_name, :role)
+  end
+  private :profile_params
+end
---
+++ db/migrate/20170327174239_create_profiles.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+class CreateProfiles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :profiles do |t|
+      t.string :given_name, null: false
+      t.string :family_name, null: false
+      t.integer :role, :integer, null: false
+      t.references :user, foreign_key: true, null: false
+
+      t.timestamps
+    end
+  end
+end
