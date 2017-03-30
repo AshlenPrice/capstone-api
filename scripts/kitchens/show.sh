72010e5c2c0f20cfbf8f9f051cffb6d1e4a655ca
ashlenprice <ashlen.price@gmail.com>
2017-03-30 18:05:08 -0400

    Clean up files
    removed example files and took out the test tokens out of the scripts.

--- scripts/kitchens/show.sh
+++ scripts/kitchens/show.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-TOKEN="BAhJIiU0ZTIwZWM5NWU1OTcxMTM3NDk0OGUxNWY2Yjg4NWRmYQY6BkVG--25236258d86fab74e351edcaac4a07ec8555257b"
 
 API="${API_ORIGIN:-http://localhost:4741}"
 URL_PATH="/kitchens/${ID}"
--- scripts/kitchens/userkitchens.sh
+++ scripts/kitchens/userkitchens.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-TOKEN="BAhJIiU5N2JkMjcyNjNmMWM3ZGFhMjA2NWRlZWYzMzE5ZTRmMQY6BkVG--b9184d980f0552f5d09a1fb80def76ae3e1acf1b"
-
 
 API="${API_ORIGIN:-http://localhost:4741}"
 URL_PATH="/userskitchen"
